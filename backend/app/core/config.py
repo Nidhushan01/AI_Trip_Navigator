a617c8cb7907a29ed4399406b5e4e699db7e4256
Nidhushan01 <nidhu1213@gmail.com>
2026-06-07 00:03:45 +0530

    cors updated

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
     cors_origins: list[str] = [
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "ai-trip-navigator-pntcept8w-nidhushans-projects.vercel.app",
     ]
 
     model_config = SettingsConfigDict(
