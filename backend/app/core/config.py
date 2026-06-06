31b905d5b9133c8258d3c459f891096e3beae422
Nidhushan01 <nidhu1213@gmail.com>
2026-06-07 00:42:15 +0530

    cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -17,6 +17,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://localhost:5173",
         "ai-trip-navigator-pntcept8w-nidhushans-projects.vercel.app",
+        "https://ai-trip-navigator.vercel.app",
     ]
 
     model_config = SettingsConfigDict(
